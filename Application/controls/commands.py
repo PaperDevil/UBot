804048d6963a1c7f29d8115a6495e22a01861cb9
PaperDevil <ketov-x@yandex.ru>
2020-05-31 22:04:43 +0300

    Встроена вопросно-ответная система на базе библиотеки Deeppavlov.

--- Application/controls/commands.py
+++ Application/controls/commands.py
@@ -1,12 +1,33 @@
+import os
+import sys
+import logging
+
+from deeppavlov import train_model
+from deeppavlov.deprecated.agents.default_agent import DefaultAgent
+from deeppavlov.deprecated.agents.processors import HighestConfidenceSelector
+from deeppavlov.deprecated.skills.pattern_matching_skill import PatternMatchingSkill
 from telebot.types import Update
 from telegram.ext import Updater, CallbackContext
 
+
 # File for recording response functions
 
+QAModel = train_model(os.path.dirname(sys.argv[0]) + "/Application/config.json")
+agent = DefaultAgent([QAModel], skills_selector=HighestConfidenceSelector())
+
 def send_help(update: Update, context: CallbackContext):
-    '''
-    :param update:
-    :param context:
-    :return:
-    '''
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Command list: ...')
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Command list: ...')
+
+def QAnswer(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=agent([update.message.text])[0])
+
+def get_indicates(update: Update, context: CallbackContext):
+    ...
+
+def register_user(update: Update, context: CallbackContext):
+    ...
+
+def get_personal_account(update: Update, context: CallbackContext):
+    ...
